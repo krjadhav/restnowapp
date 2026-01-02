d7f4e0b4ad77b6de41affa3fd77ec539251b5c6b
Kausthub Jadhav <j@dhav.me>
2026-01-02 23:19:49 +0530

    revert changes

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router basename="/restnowebsite">
+    <Router basename="/restnowapp">
       <div className="min-h-screen">
         <Navigation />
         <main>
